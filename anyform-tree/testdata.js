f5ced120213330e3929c8baf013a52bc4276d6c6
Florian Rüberg <florian.rueberg@gmail.com>
2017-11-18 14:45:29 +0100

    Start tree redesign

---
+++ anyform-tree/testdata.js
@@ -0,0 +1,66 @@
+export let testdata = [
+{
+    id: "A",
+    title: "A",
+    contains: [
+        {
+        id: "A1",
+        title: "A1",
+        },
+        {
+        id: "A2",
+        title: "A2",
+        },
+        {
+        id: "A3",
+        title: "A3",
+        },
+    ]
+},
+{
+    id: "B",
+    title: "B",
+    contains: [
+        {
+        id: "B1",
+        title: "B1",
+        },
+        {
+        id: "B2",
+        title: "B2",
+        },
+    ]
+},
+{
+    id: "C",
+    title: "C",
+    contains: [
+        {
+        id: "C1",
+        title: "C1",
+        contains: [
+            {
+                id: "C1x",
+                title: "C1x",
+            },
+            {
+                id: "C1y",
+                title: "C1y",
+            },
+            {
+                id: "C1z",
+                title: "C1z",
+            },
+            {
+                id: "C1zz",
+                title: "C1zz",
+            },
+            {
+                id: "C1zzz",
+                title: "C1zzz",
+            },
+        ]
+        },
+    ]
+}
+];
--- docs/index.js
+++ docs/index.js
@@ -1,3 +1,10 @@
-require('anyform-core/index.js');
-console.log(require('anyform-default/generator.yaml'));
-require('anyform-tree/App');
+//require('anyform-core/index.js');
+//console.log(require('anyform-default/generator.yaml'));
+//require('anyform-tree/App');
+
+import ReactDOM from "react-dom";
+import React from "react";
+import {Container} from 'anyform-tree';
+
+ReactDOM.render(<Container />, document.getElementById("root"));
+
