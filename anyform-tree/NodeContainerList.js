834888419e15eaea1daecd82539b6596375d94f5
Florian Rüberg <florian.rueberg@gmail.com>
2017-11-19 15:20:04 +0100

    Continue tree

--- anyform-tree/NodeContainerList.js
+++ anyform-tree/NodeContainerList.js
@@ -13,38 +13,52 @@ const getSourceItem = (props) => props.current;
 }))
 class NodeContainer extends Component {
 
-	render() {
-		let { options, current, previous, isLast, index, dragging, isDragging } = this.props;
+	renderTargets(cx) {
+		let { current, previous, isLast, index, dragging, isDragging, isDraggingParent } = this.props;
 
-		const cx = classNames.bind(options.styles);
-		const node = options.node(current, index);
+		if (!dragging || isDragging || isDraggingParent) return [];
 
-		const content = [
-			this.props.connectDragSource(<div className={cx('node')}>{node}</div>),
-			<div className={cx('node-preview-top')}></div>,
-			<div className={cx('node-preview-bottom')}></div>
-		];
+		// Build drop parameter
+		// Use real targets
 
-		if (isDragging && dragging !== current) {
-			if (dragging !== previous) content.push(<div className={cx('node-target-top')}></div>);
-			if (isLast) content.push(<div className={cx('node-target-bottom')}></div>);
+		let content = [];
+		if (dragging !== previous) {
+			content.push(<div className={cx('node-target-top')}></div>);
+			content.push(<div className={cx('node-preview-top')}></div>);
+		}
+		if (isLast) {
+			content.push(<div className={cx('node-target-bottom')}></div>);
+			content.push(<div className={cx('node-preview-bottom')}></div>);
 		}
 
-		content.concat(options.containsNormalized(current).map(({id, value}) => {
+		return content;
+	}
 
-			return <div className={cx('node-content-group')}>
-				<div className={cx('group-label')}>
-					{options.containsLabel(id, current)}
-				</div>
-				<NodeContainerList list={value} isDragging={isDragging} />
-			</div>;
-		}));
+	renderChildGroup(cx, {id, value}) {
+		let isDragging = this.props.isDragging || this.props.isDraggingParent;
+		let label = this.props.options.containsLabel(id, current);
+		
+		return <div className={cx('node-content-group')}>
+			{label && <div className={cx('group-label')}>{label}</div>}
+			<NodeContainerList list={value} isDragging={isDragging} />
+		</div>;
+	}
 
-		return <div className={cx('node-container')}>{content}</div>;
+	render() {
+		let { options, current, index, isDragging } = this.props;
+		const cx = classNames.bind(options.styles);
+
+		let node = <div className={cx('node')}>{options.node(current, index)}</div>;
+		
+		return <div className={cx('node-container')}>
+			{this.props.connectDragSource(node)}
+			{options.containsNormalized(current).map((g) => this.renderChildGroup(cx, g))}
+			{this.renderTargets(cx)}
+		</div>;
 	}
 }
 
-export const NodeContainerList = ({list, isDragging}) => <div>{list.map((_, i) => {
+export const NodeContainerList = ({list, isDragging, path}) => <div>{list.map((_, i) => {
 	var props = {
 		current:  list[i],  
 		previous: list[i-1],
